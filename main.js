319704a36a423a4beac3f7e99ac073026fc17944
palaarda <gametime.plz@outlook.com>
2023-12-31 01:16:47 +0300

    first

---
+++ main.js
@@ -0,0 +1,27 @@
+const city = document.getElementById('city');
+const country = document.getElementById('country');
+const ip = document.getElementById('ipS');
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+const regioncode = document.getElementById('regioncode');
+const regionname = document.getElementById('regionname');
+const zipcode = document.getElementById('zipcode');
+
+async function getLoc() {
+    const con = await fetch('https://api.collectapi.com/ip/ipToLocation?data.ip=95.8.131.139', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'apikey 0lZtqW8Nql62CDZ4KHOqwr:6U2Jnu8SY7sFLRq9wePzIv'
+        }
+    });
+    const dataJson = await con.json();
+    const result = dataJson.result
+    console.log(result);
+}
+getLoc();
+
+function getVal(){
+    const getValue = document.getElementById('ipInput').value;
+    console.log(getValue);
+}
